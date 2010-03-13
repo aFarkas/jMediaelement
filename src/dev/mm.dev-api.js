f560cb01bb8def5a56ca4dc85797f14f79a64e8f
aFarkas <info@corrupt-system.de>
2010-03-13 14:55:02 +0100

    added sample files for ading new mediaplayer to the project

---
+++ src/dev/mm.dev-api.js
@@ -0,0 +1,111 @@
+/**!
+ * Part of the jMediaelement-Project | http://github.com/aFarkas/jMediaelement
+ * @author Alexander Farkas
+ * Copyright 2010, Alexander Farkas
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+
+/*
+ * How to implement new plugins for the jMediaelement-Project
+ * watchout for the keyword 'yourPlugin'
+ */
+
+(function($){
+		
+	/*
+	 * this object will be used as a prototype for your plugin
+	 * this.apiElem refers to your embedded element
+	 * this.html5elem reffers to the mediaelement
+	 */ 
+	var yourPlugin = {
+		/*
+		 * _init will be called right after the apiElem was embedded
+		 */
+		_init: function(){
+			
+			//this._trigger('mmAPIReady');
+		},
+		/*
+		 * return a float in secondes
+		 */
+		getDuration: function(){
+			
+		},
+		/* 
+		 * return boolean
+		 */
+		isPlaying: function(){
+			
+		},
+		play: function(){
+			
+		},
+		pause: function(){
+			
+		},
+		/*
+		 * works as getter/setter
+		 * allowed is a float between 0 - 100
+		 */
+		volume: function(v){
+			
+		},
+		/*
+		 * works as a getter/setter
+		 * time in secondes
+		 */
+		currentTime: function(sec){
+			
+		},
+		/*
+		 * works as a getter/setter
+		 * use boolean
+		 */
+		muted: function(state){
+			
+		},
+		/*
+		 * changes the media: retrieves the source of the main media-file and the optional poster-src
+		 * if $.attr(this.html5elem, 'autoplay') is true, the playback has to be start immediatley otherwise it shouldn´t start
+		 */
+		_mmload: function(src, poster){
+			
+		},
+		
+		/*
+		 * the following methods / properties are optional
+		 */
+		_videoFullscreen: false, //true
+		enterFullScreen: function(){},
+		exitFullScreen: function(){},
+		_setActive: function(){},
+		_setInactive: function(){}
+	};
+	
+	/*
+	 * additionaly you have to trigger the following events by using this._trigger(event);
+	 * event is on object or the event name
+	 */
+	/*
+	 * simple events:
+	 * 	required:
+	 * 		mmAPIReady, play, pause, ended, playing
+	 * 	optional:
+	 * 		waiting
+	 * 
+	 * compley events:
+	 * 	required:
+	 * 		loadedmeta (with duration in secondes)
+	 * 		timechange (width time in secondes)
+	 * 		mute (with isMuted as boolean)
+	 * 		
+	 * 	optional:
+	 * 		volumelevelchange: (volumelevel on 0 - 100 basis)
+	 * 		progresschange: (with loaded (number), lengthComputable (boolean) and if lengthComputable total and relLoaded)
+	 * 		
+	 */
+	
+	// now add your object as a prototype for the video/audio element
+	$.multimediaSupport.add('yourPlugin', 'video', yourPlugin);
+	$.multimediaSupport.add('yourPlugin', 'audio', yourPlugin);
+})(jQuery);
---
+++ src/dev/mm.dev-embed.js
@@ -0,0 +1,44 @@
+/**!
+ * Part of the jMediaelement-Project | http://github.com/aFarkas/jMediaelement
+ * @author Alexander Farkas
+ * Copyright 2010, Alexander Farkas
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+
+/*
+ * How to implement new plugins for the jMediaelement-Project
+ * watchout for the keyword 'yourPlugin'
+ */
+
+(function($){
+	//options
+	$.extend($.fn.mediaElementEmbed.defaults, 
+			{yourPlugin: {}}
+		)
+	;
+	
+	// this object will be used as a prototype for your plugin
+	var yourPlugin = {
+		isTechAvailable: true,
+		canPlayCodecs: ['theora', 'vorbis'],
+		canPlayContainer: ['application/ogg', 'audio/ogg', 'video/ogg', 'audio/x-ogg', 'video/x-ogg'],
+		_embed: function(src, id, cfg, fn){
+			// generate a plugin element with the id, src and cfg and give it a width/height of 100%
+			//cfg.loop, cfg.autoplay, cfg.autobuffer, cfg.controls
+			
+			//append the generated element to this.visualElem (This is an jQuery-Object)
+			//this.visualElem.append(elem);
+			//call the fn-function with your successful generated element as an argument
+			//fn(elem);
+		}
+	};
+	
+	// add a mimetype extension mapping, if it isn´t done yet (call $.multimediaSupport._showMimeTypes() in your firebug console to see all registered/mapped mimetypes)
+	// (if you support multiple mimetypes and all of them use the same extensions. you only have to register one of these mime-types)
+	$.multimediaSupport.registerMimetype('audio', {
+		'application/ogg': ['ogg', 'ogm', 'oga']
+	});
+	// now add your object as a prototype for the video/audio element
+	$.multimediaSupport.add('yourPlugin', 'video', yourPlugin);
+	$.multimediaSupport.add('yourPlugin', 'audio', yourPlugin);
+})(jQuery);

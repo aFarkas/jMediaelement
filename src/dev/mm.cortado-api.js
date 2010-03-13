e78fd87af4bb32d63b627915ac07ff7540b7b898
aFarkas <info@corrupt-system.de>
2010-03-13 14:45:24 +0100

    added a cortado dev version

--- src/dev/mm.cortado-api.js
+++ src/dev/mm.cortado-api.js
@@ -1,8 +1,10 @@
-/**
- * @author trixta
+/**!
+ * Part of the jMediaelement-Project | http://github.com/aFarkas/jMediaelement
+ * @author Alexander Farkas
+ * Copyright 2010, Alexander Farkas
+ * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 (function($){
-	
 	var playTimer = (function(){
 		var timer, 
 			fns = []
@@ -18,6 +20,7 @@
 			add: function(fn){
 				if($.inArray(fn, fns) === -1){
 					fns.push(fn);
+					setTimeout(fn, 300);
 				}
 				if(fns.length === 1){
 					timer = setInterval(invoke, 1000);
@@ -40,34 +43,41 @@
 				lastTime,
 				updateCurrentTime 	= function(){
 							if(api.apiElem.currentTime !== lastTime){
-								var e = {
-									type: 'timechange',
-									time: api.apiElem.currentTime
-								};
-								if( api._duration ){
-									e.duration = api._duration;
-									e.timeProgress = e.time / e.duration * 100;
+								var e = {};
+								lastTime = api.apiElem.currentTime;
+								if(lastTime > 0){
+									e.type = 'timechange';
+									e.time = lastTime;
+									if(!api._isPlayingState){
+										api._isPlayingState = true;
+										api._trigger('playing');
+									}
+									if( api._duration ){
+										e.duration = api._duration;
+										e.timeProgress = e.time / e.duration * 100;
+									}
+								} else {
+									e.type = 'ended'; 
 								}
-								lastTime = e.time;
 								api._trigger(e);
 							}
 						}
 			;
-			
 			setTimeout(function(){
 				//autoPlay
-				api._duration = parseInt(api.html5elem.getAttribute('data-duration'), 10);
-				api._isPausedState = (api.apiElem.getParameter('autoPlay') === 'false');
+				api._duration = parseInt(api.html5elem.getAttribute('data-duration'), 10) || 2000;
+				var autoplay = (api.apiElem.getParameter('autoPlay') !== 'false');
 				api._isMuted = (api.apiElem.getParameter('audio') === 'false');
 				lastTime = api.apiElem.currentTime;
-				
-				
-				
+				if(autoplay){
+					playTimer.add(updateCurrentTime);
+				}
 				$(api.html5elem)
 					.bind('play', function(){
 						playTimer.add(updateCurrentTime);
 					})
 					.bind('pause ended', function(){
+						api._isPlayingState = false;
 						playTimer.remove(updateCurrentTime);
 					})
 				;
@@ -75,42 +85,41 @@
 				api._trigger({
 					type: 'mmAPIReady',
 					noVolume: true,
-					noProgress: true,
-					noDuration: !(api._duration)
+					noProgress: true
 				});
 				
 				api._trigger({
 					type: 'loadedmeta',
 					duration: api._duration
 				});
-				if(!api._isPausedState){
-					api._trigger('play');
-				}
 			}, 0);
 		},
 		getDuration: function(){
-			return api._duration;
+			return this._duration;
 		},
-		isPaused: function(){
-			return this._isPausedState;
+		isPlaying: function(){
+			return this._isPlayingState;
 		},
 		play: function(){
 			this.apiElem.play();
-			this._isPausedState = false;
 			this._trigger('play');
 		},
 		pause: function(){
 			this.apiElem.pause();
-			this._isPausedState = true;
 			this._trigger('pause');
 		},
-		currentTime: function(sec){
-			if(arguments.length){
-				if(this._duration){
-					this.apiElem.doSeek(sec / this._duration);
-				}
+		// not available
+		volume: function(v){
+			if (!isFinite(v)) {
+				return 100;
 			}
-			return this.apiElem.currentTime;
+		},
+		currentTime: function(sec){
+			if(isFinite(sec)){
+				this.apiElem.doSeek(sec / this._duration);
+			} else {
+				return this.apiElem.currentTime;
+			}
 		},
 		muted: function(state){
 			if(!arguments.length){
@@ -121,8 +130,16 @@
 			this._isMuted = state;
 			var e = {type: 'mute', isMuted: state};
 			this._trigger(e);
+		},
+		_mmload: function(src, poster){
+			var autoplay = $.attr(this.html5elem, 'autoplay');
+			this.apiElem.setParam('autoPlay', ''+autoplay);
+			this.apiElem.setParam('image', $.attr(this.html5elem, 'poster') || '');
+			this.apiElem.setParam("url", uri);
+			this.apiElem.restart();
 		}
 	};
 	
 	$.multimediaSupport.add('cortado', 'video', cortado);
+	$.multimediaSupport.add('cortado', 'audio', cortado);
 })(jQuery);

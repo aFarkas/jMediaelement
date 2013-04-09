40133c721f261194f6ea3459ccdbd904545173f4
aFarkas <info@corrupt-system.de>
2013-04-09 18:01:36 +0200

    version 2.0.4

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			full: {
+                src: ['src/jme.js', 'src/jme.fullscreen.js', 'src/jme.track.js'],
+                dest: 'demo/js/jme.full.js'
+            },
+            base: {
+                src: ['src/jme.js'],
+                dest: 'demo/js/jme.base.js'
+            }
+		},
+		uglify: {
+			options: {
+				banner: ''
+			},
+			full: {
+                src: 'demo/js/jme.full.js',
+                dest: 'demo/js/jme.full.min.js'
+            },
+            base: {
+                src: 'demo/js/jme.base.js',
+                dest: 'demo/js/jme.base.min.js'
+            },
+            fullscreen: {
+                src: 'src/jme.fullscreen.js',
+                dest: 'demo/js/plugins/jme.fullscreen.min.js'
+            },
+            track: {
+                src: 'src/jme.track.js',
+                dest: 'demo/js/plugins/jme.track.min.js'
+            }
+		},
+		watch: {
+			files: ['src/jme.js', 'src/jme.fullscreen.js', 'src/jme.track.js'],
+			tasks: 'default'
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', ['concat', 'uglify']);
+
+};
--- demo/js/jme.full.js
+++ demo/js/jme.full.js
@@ -1328,4 +1328,516 @@
 		started = true;
 	};
 	
+})(jQuery);;(function($){
+	var btnStructure = '<button class="{%class%}"><span class="jme-icon"></span><span class="jme-text">{%text%}</span></button>';
+	var pseudoClasses = 'pseudoClasses';
+	
+	//taken from http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugin/
+	$.jme.fullscreen = (function() {
+		var parentData;
+		var frameData;
+		var doc = document.documentElement;
+		
+		var fullScreenApi = {
+			supportsFullScreen: Modernizr.prefixed('fullscreenEnabled', document, false) || Modernizr.prefixed('fullScreenEnabled', document, false),
+			isFullScreen: function() { return false; },
+			requestFullScreen: function(elem){
+				var tmpData;
+				parentData = [];
+				$(elem).parentsUntil('body').each(function(){
+					var pos =  $.css(this, 'position');
+					var left = this.scrollLeft;
+					var top = this.scrollTop;
+					var changed;
+					tmpData = {elemStyle: this.style, elem: this};
+					if(pos !== 'static'){
+						changed = true;
+						tmpData.pos = tmpData.elemStyle.position;
+						this.style.position = 'static';
+					}
+					if(left){
+						changed = true;
+						tmpData.left = left;
+					}
+					if(top){
+						changed = true;
+						tmpData.top = top;
+					}
+					if(changed){
+						parentData.push(tmpData);
+					}
+				});
+				frameData = false;
+				try {
+					frameData = {elemStyle: frameElement.style, elem: frameElement, css: {}};
+					frameData.css.position = frameData.elemStyle.position;
+					frameData.elemStyle.position = 'fixed';
+					$.each(['top', 'left', 'right', 'bottom'], function(i, name){
+						frameData.css[name] = frameData.elemStyle[name];
+						frameData.elemStyle[name] = '0px';
+					});
+					$.each(['height', 'width'], function(i, name){
+						frameData.css[name] = frameData.elemStyle[name];
+						frameData.elemStyle[name] = '100%';
+					});
+				} catch(er){
+					frameData = false;
+				}
+				
+				tmpData = null;
+			},
+			cancelFullScreen: function(){
+				if(parentData){
+					$.each(parentData, function(i, data){
+						if('pos' in data){
+							data.elemStyle.position = data.pos;
+						}
+						if(data.left){
+							data.elem.scrollLeft = data.left;
+						}
+						if(data.top){
+							data.elem.scrollTop = data.top;
+						}
+						data = null;
+					});
+					parentData = [];
+				}
+				if(frameData){
+					$.each(frameData.css, function(name, value){
+						frameData.elemStyle[name] = value;
+					});
+					frameData = false;
+				}
+			},
+			eventName: 'fullscreenchange',
+			exitName: 'exitFullscreen',
+			requestName: 'requestFullscreen',
+			elementName: 'fullscreenElement',
+			enabledName: ''
+		};
+		
+		fullScreenApi.cancelFullWindow = fullScreenApi.cancelFullScreen;
+		fullScreenApi.requestFullWindow = fullScreenApi.requestFullScreen;
+
+		// update methods to do something useful
+		if (fullScreenApi.supportsFullScreen) {
+			fullScreenApi.enabledName = fullScreenApi.supportsFullScreen;
+			fullScreenApi.exitName = Modernizr.prefixed("exitFullscreen", document, false) || Modernizr.prefixed("cancelFullScreen", document, false);
+			fullScreenApi.elementName = Modernizr.prefixed("fullscreenElement", document, false) || Modernizr.prefixed("fullScreenElement", document, false);
+			fullScreenApi.supportsFullScreen = !!fullScreenApi.supportsFullScreen;
+			if(fullScreenApi.elementName != 'fullscreenElement' || fullScreenApi.exitName != 'exitFullscreen' || fullScreenApi.enabledName != 'fullscreenEnabled'){
+				$.each(Modernizr._domPrefixes, function(i, prefix){
+					var requestName = prefix+'RequestFullscreen';
+					if((requestName in doc) || ((requestName = prefix+'RequestFullScreen') && (requestName in doc))){
+						fullScreenApi.eventName = prefix + 'fullscreenchange';
+						fullScreenApi.requestName = requestName;
+						return false;
+					}
+				});
+			}
+			
+			fullScreenApi.isFullScreen = function() {
+				return document[fullScreenApi.elementName];
+			};
+			fullScreenApi.requestFullScreen = function(el) {
+				return el[fullScreenApi.requestName]();
+			};
+			fullScreenApi.cancelFullScreen = function() {
+				return document[fullScreenApi.exitName]();
+			};
+		}
+		
+		if(!window.Modernizr || !('fullscreen' in Modernizr)){
+			$('html').addClass(fullScreenApi.supportsFullScreen ? 'fullscreen' : 'no-fullscreen');
+		}
+		
+		if(!fullScreenApi.supportsFullScreen && window.parent != window){
+			(function(){
+				try{
+					var a = window.frameElement.style;
+				} catch(er){
+					$('html').addClass('no-fullwindow');
+				}
+			})();
+			
+		}
+		
+
+		return fullScreenApi;
+	})();
+	
+	$.jme.defineProp('fullscreen', {
+		set: function(elem, value){
+			var data = $.jme.data(elem);
+			
+			if(!data || !data.player){return 'noDataSet';}
+			if(value){
+				if(data.player.hasClass($.jme.classNS+'player-fullscreen')){return 'noDataSet';}
+
+				data.scrollPos = {
+					top: $(window).scrollTop(),
+					left: $(window).scrollLeft()
+				};
+
+				$(document)
+					.unbind('.jmefullscreen')
+					.bind('keydown.jmefullscreen', function(e){
+						if(e.keyCode == 27){
+							data.player.jmeProp('fullscreen', false);
+							return false;
+						}
+						if(e.keyCode === 32 && !('form' in e.target)){
+							data.media.jmeFn('togglePlay');
+							return false;
+						}
+					})
+				;
+				
+				
+				if(value == 'fullwindow'){
+					$.jme.fullscreen.requestFullWindow(data.player[0]);
+				} else {
+					try {
+						$.jme.fullscreen.requestFullScreen(data.player[0]);
+					} catch(er){}
+				}
+
+				
+				$('html').addClass($.jme.classNS+'has-media-fullscreen');
+
+				data.player.addClass($.jme.classNS+'player-fullscreen');
+
+				data.media.addClass($.jme.classNS+'media-fullscreen');
+				
+				$('.jme-default-control-bar', data.player).focus();
+
+				if($.jme.fullscreen.supportsFullScreen){
+					$(document)
+						.bind($.jme.fullscreen.eventName+'.jmefullscreen', function(e){
+							var fullScreenElem = $.jme.fullscreen.isFullScreen();
+							if(fullScreenElem && elem == fullScreenElem){
+								$(elem).triggerHandler('playerdimensionchange', ['fullscreen']);
+							} else {
+								data.player.jmeProp('fullscreen', false);
+							}
+						})
+					;
+
+				}
+				data.player.triggerHandler('playerdimensionchange', ['fullwindow']);
+				
+			} else {
+				if(!data.player.hasClass($.jme.classNS+'player-fullscreen')){return 'noDataSet';}
+				$(document).unbind('.jmefullscreen');
+				$('html').removeClass($.jme.classNS+'has-media-fullscreen');
+				data.player.removeClass($.jme.classNS+'player-fullscreen');
+				data.media.removeClass($.jme.classNS+'media-fullscreen');
+				if($.jme.fullscreen.isFullScreen()){
+					try {
+						$.jme.fullscreen.cancelFullScreen();
+					} catch(er){}
+				} else {
+					$.jme.fullscreen.cancelFullWindow();
+				}
+
+
+				data.player.triggerHandler('playerdimensionchange');
+				if(data.scrollPos){
+					$(window).scrollTop(data.scrollPos.top);
+					$(window).scrollLeft(data.scrollPos.left);
+					delete data.scrollPos;
+				}
+			}
+			return 'noDataSet';
+		},
+		get: function(elem){
+			var data = $.jme.data(elem);
+			if(!data || !data.player){return;}
+			var fs = data.player.hasClass($.jme.classNS+'player-fullscreen');
+			if(!fs){return false;}
+			return $.jme.fullscreen.isFullScreen() || 'fullwindow';
+		}
+	});
+	
+	$.jme.defineProp('autoplayfs');
+
+	$.jme.registerPlugin('fullscreen', {
+		pseudoClasses: {
+			enter: 'state-enterfullscreen',
+			exit: 'state-exitfullscreen'
+		},
+		options: {
+			fullscreen: true,
+			autoplayfs: false
+		},
+		structure: btnStructure,
+		text: 'enter fullscreen / exit fullscreen',
+		_create: function(control, media, base){
+			var textFn = $.jme.getButtonText(control, [this[pseudoClasses].enter, this[pseudoClasses].exit]);
+			var updateControl = function(){
+				textFn(base.hasClass($.jme.classNS+'player-fullscreen') ? 1 : 0);
+			};
+			var options = this.options;
+			var addDoubbleClick = function(){
+				$(base.data('jme').controlElements)
+					.filter('.jme-default-media-overlay')
+					.off('.dblfullscreen')
+					.on('dblclick.dblfullscreen', function(e){
+						base.jmeProp('fullscreen', !base.jmeProp('fullscreen'));
+					})
+				;
+			};
+			
+			base.on('controlsadded', addDoubbleClick);
+			
+			base.bind('playerdimensionchange', updateControl);
+			
+			control.bind((control.is('select')) ? 'change' : 'click', function(){
+				var value = base.hasClass($.jme.classNS+'player-fullscreen') ? false : options.fullscreen;
+				base.jmeProp('fullscreen', value);
+				if(value && options.autoplayfs){
+					media.jmeFn('play');
+				}
+			});
+			addDoubbleClick();
+			updateControl();
+		}
+	});
+})(jQuery);
+;(function($){
+	var btnStructure = '<button class="{%class%}"><span class="jme-icon"></span><span class="jme-text">{%text%}</span></button>';
+	var pseudoClasses = 'pseudoClasses';
+	/**
+	 * Added captions Plugin
+	 * @author mderting
+	 */
+	
+	$.jme.ButtonMenu = function(button, menu, clickHandler){
+		
+		this.button = $(button).attr({'aria-haspopup': 'true'});
+		
+		this.clickHandler = clickHandler;
+		
+		this.toggle = $.proxy(this, 'toggle');
+		this.keyIndex = $.proxy(this, 'keyIndex');
+		this._buttonClick = $.proxy(this, '_buttonClick');
+		
+		
+		this.addMenu(menu);
+		this._closeFocusOut();
+		this.button.bind('click', this.toggle);
+		
+	};
+	
+	$.jme.ButtonMenu.prototype = {
+		addMenu: function(menu){
+			if(this.menu){
+				this.menu.remove();
+			}
+			this.menu = $(menu);
+			this.buttons = $('button', this.menu);
+			this.menu.insertAfter(this.button);
+			this.menu
+				.bind('keydown', this.keyIndex)
+				.delegate('button', 'click', this._buttonClick)
+			;
+		},
+		_closeFocusOut: function(){
+			var that  = this;
+			var timer;
+			var stopFocusOut = function(){
+					clearTimeout(timer);
+					setTimeout(function(){
+						clearTimeout(timer);
+					}, 9);
+				};
+			this.menu
+				.parent()
+				.bind('focusin', stopFocusOut)
+				.bind('mousedown', stopFocusOut)
+				.bind('focusout', function(e){
+					timer = setTimeout(function(){
+						that.hide();
+					}, 40);
+				})
+			;
+		},
+		_buttonClick: function(e){
+			this.clickHandler(this.buttons.index(e.currentTarget), e.currentTarget);
+			this.hide();
+		},
+		keyIndex: function(e){
+			var dir = (e.keyCode == 40) ? 1 : (e.keyCode == 38) ? -1 : 0;
+			if(dir){
+				var buttons = this.buttons.not(':disabled');
+				var activeButton = buttons.filter(':focus');
+				
+				activeButton = buttons[buttons.index(activeButton) + dir] || buttons.filter(dir > 0 ? ':first' : ':last');
+				activeButton.focus();
+				e.preventDefault();
+			}
+		},
+		show: function(){
+			if(this.isVisible){return;}
+			var buttons = this.buttons.not(':disabled');
+			this.isVisible = true;
+			this.menu.addClass('visible-menu');
+			try {
+				this.activeElement = document.activeElement || this.button[0];
+			} catch(er){
+				this.activeElement = this.button[0];
+			}
+			
+			setTimeout(function(){
+				$(buttons.filter('[aria-checked="true"]')[0] || buttons[0]).focus();
+			}, 60);
+		},
+		toggle: function(){
+			this[this.isVisible ? 'hide' : 'show']();
+		},
+		hide: function(){
+			if(!this.isVisible){return;}
+			this.isVisible = false;
+			this.menu.removeClass('visible-menu');
+			if(this.activeElement){
+				try {
+					this.activeElement.focus();
+				} catch(er){}
+			}
+			this.activeElement = false;
+		}
+	};
+	
+	var showKinds = {subtitles: 1, caption: 1};
+	var getTrackMenu = function(tracks){
+		var items = $.map(tracks, function(track){
+				var className = (track.kind == 'caption') ? 'caption-type' : 'subtitle-type';
+				var lang = track.language;
+				lang = (lang) ? ' <span class="track-lang">'+ lang +'</span>' : '';
+				return '<li class="'+ className +'" role="presentation"><button role="menuitemcheckbox">'+ track.label + lang +'</button></li>';
+			})
+		;
+		return '<div><ul>' + items.join('') +'</ul></div>';
+	};
+	
+	
+	$.jme.registerPlugin('captions', {
+		pseudoClasses: {
+			enabled: 'state-captionsenabled',
+			disabled: 'state-captionsdisabled',
+			noTrack: 'no-track',
+			hasTrack: 'has-track',
+			menu: 'subtitle-menu'
+		},
+		structure: btnStructure,
+		text: 'subtitles menu',
+		_create: function(control, media, base, options){
+			var that = this;
+			
+			var trackElems = media.find('track');
+			var btnTextElem = $('span.jme-text, +label span.jme-text', control);
+			if(!btnTextElem[0]){
+				btnTextElem = control;
+			}
+			btnTextElem.html(that.text);
+			if(!trackElems.length){
+				control.prop('disabled', true);
+				base.addClass(that[pseudoClasses].noTrack);
+			} else {
+				base.addClass(that[pseudoClasses].hasTrack);
+			}
+			
+			
+			$.webshims.ready('track', function(){
+				var menuObj;
+				var tracks = [];
+				var textTracks = media.prop('textTracks');
+				var throttledUpdate = (function(){
+					var timer;
+					var triggerTimer;
+					return function(e){
+						clearTimeout(timer);
+						clearTimeout(triggerTimer);
+						if(e.type == 'updatesubtitlestate'){
+							triggerTimer = setTimeout(function(){
+								media.trigger('updatetracklist');
+							}, 0);
+						}
+						timer = setTimeout(updateTrackMenu, 19);
+					};
+				})();
+				
+				function createSubtitleMenu(menu){
+					if(!menuObj){
+						menuObj = new $.jme.ButtonMenu(control, menu, function(index, button){
+							if($.attr(button, 'aria-checked') == 'true'){
+								tracks[index].mode = 'disabled';
+							} else {
+								$.each(tracks, function(i, track){
+									track.mode = (i == index) ? 'showing' : 'disabled';
+								});
+							}
+							media.prop('textTracks');
+							updateMode();
+						});
+					} else {
+						menuObj.addMenu(menu);
+					}
+					
+					updateMode();
+				}
+				
+				function updateMode(){
+					$('button', menuObj.menu).each(function(i){
+						$.attr(this, 'aria-checked', (tracks[i].mode == 'showing') ? 'true' : 'false');
+					});
+				}
+				
+				function updateTrackMenu(){
+					tracks = [];
+					$.each(textTracks, function(i, track){
+						if(showKinds[track.kind] && track.readyState != 3){
+							tracks.push(track);
+						}
+					});
+					if(tracks.length){
+						createSubtitleMenu('<div class="'+that[pseudoClasses].menu +'" >'+ (getTrackMenu(tracks)) +'</div>');
+						if(!base.hasClass(that[pseudoClasses].hasTrack) || base.hasClass(that[pseudoClasses].noTrack)){
+							control.prop('disabled', false);
+							base
+								.addClass(that[pseudoClasses].hasTrack)
+								.removeClass(that[pseudoClasses].noTrack)
+								.triggerHandler('controlschanged')
+							;
+						}
+					} else if(!base.hasClass(that[pseudoClasses].noTrack) || base.hasClass(that[pseudoClasses].hasTrack)){
+						control.prop('disabled', true);
+						base
+							.addClass(that[pseudoClasses].noTrack)
+							.removeClass(that[pseudoClasses].hasTrack)
+							.triggerHandler('controlschanged')
+						;
+					}
+				}
+				if(!textTracks){
+					textTracks = [];
+					updateTrackMenu();
+				} else {
+					updateTrackMenu();
+					$([textTracks]).on('addtrack removetrack', throttledUpdate);
+					base.bind('updatesubtitlestate', throttledUpdate);
+					media.bind('updatetrackdisplay', (function(){
+						var timer;
+						return function(){
+							clearTimeout(timer);
+							timer = setTimeout(updateMode, 20);
+						};
+					})());
+				}
+				
+			});
+			
+		}
+		
+	});
 })(jQuery);
